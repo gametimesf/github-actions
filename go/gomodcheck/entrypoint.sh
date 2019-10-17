635bca47b063fd5be71e3243d342b8ac85457bd9
Andy Janata <andy.janata@gametime.co>
2019-10-17 12:28:05 -0700

    fix git

--- go/gomodcheck/entrypoint.sh
+++ go/gomodcheck/entrypoint.sh
@@ -15,7 +15,10 @@ case $ACTION in
 esac
 
 cd /
+git config --global url."https://${ORG_GITHUB_TOKEN}@github.com/gametimesf".insteadOf "https://github.com/gametimesf"
 go get github.com/gametimesf/ops/gomodcheck
+
+export GITHUB_ACCESS_TOKEN="$ORG_GITHUB_TOKEN"
 
 cd "${GO_WORKING_DIR:-.}"
 
