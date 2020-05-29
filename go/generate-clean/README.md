53e1dbac386ea1c75efe5f3809e88292107456a3
Andy Janata <andy.janata@gametime.co>
2020-05-29 11:25:29 -0700

    action to verify go generate is clean (#3)
    action to verify go generate is clean

---
+++ go/generate-clean/README.md
@@ -0,0 +1,25 @@
+# go generate
+
+Runs `go generate` and verifies that no files changed as a result.
+
+It only supports go-swagger as the valid target of `go generate`. Feel free to add support for more as needed.
+
+
+```yml
+on:
+  pull_request:
+    types: [opened, synchronize, reopened, ready_for_review]
+
+name: Go-Checks
+jobs:
+  check-go:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@master
+    - name: gofmt
+      uses: gametimesf/github-actions/go/generate-clean@v0.5.0
+      env:
+        GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+        # this token must have access to the entire organization
+        ORG_GITHUB_TOKEN: ${{ secrets.ORG_GITHUB_TOKEN }}
+```
---
+++ go/generate-clean/entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e
+
+git config --global url."https://${ORG_GITHUB_TOKEN}@github.com/gametimesf".insteadOf "https://github.com/gametimesf"
+export PATH="$PATH:/"
+
+echo "Downloading go-swagger..."
+
+download_url=$(curl -s https://api.github.com/repos/go-swagger/go-swagger/releases/latest | \
+  jq -r '.assets[] | select(.name | contains("'"$(uname | tr '[:upper:]' '[:lower:]')"'_amd64")) | .browser_download_url')
+curl -o /swagger -L'#' "$download_url"
+chmod +x /swagger
+
+cd "${GO_WORKING_DIR:-.}"
+
+set +e
+
+# run go generate
+go generate ./...
+SUCCESS=$?
+
+# Exit if `go generate` fails.
+if [ $SUCCESS -ne 0 ]; then
+  echo "go generate failed to run"
+  exit $SUCCESS
+fi
+
+# check if any files changed
+# Y U NO WORK?!?!
+# git diff-index --quiet HEAD
+git status | grep --quiet "nothing to commit, working tree clean"
+SUCCESS=$?
+if [ $SUCCESS -eq 0 ]; then
+  echo "no files changed"
+  exit 0
+fi
+
+echo "files changed"
+git status
+
+# Post results back as comment.
+COMMENT="#### \`go generate\`
+Please run \`go generate ./...\` and commit any changes.
+
+$(git status)
+"
+PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
+COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
+curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
+
+exit $SUCCESS
